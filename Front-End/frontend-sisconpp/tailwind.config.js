e82d83025f0e9fe204ac064c2f7dcdf4e42ac7d4
Kayane  Alebrante <alebrante.kayane22@gmail.com>
2024-12-20 10:12:53 -0300

    fiz: Configuração global de cores
    Adicionando a configuração global das cores do sistema, adicionado cor de fundo

--- Front-End/frontend-sisconpp/tailwind.config.js
+++ Front-End/frontend-sisconpp/tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js, ts, jsx, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: "Roboto, sans-serif",
+      },
+      
+      colors:{
+        green:{
+          500: "#1d3728",
+          200: "#7dbc97",
+        },
+
+        white:{
+          200: "#f9f3d5",
+          100: "#fcfbe5",
+        },
+
+        brown:{
+          500: "#783e19",
+          400: "#c5692a",
+          300: "#b99471",
+        },
+
+        black: {
+          500: "#000000",
+        },
+
+        alertColor:{
+          erro: "#b00005",
+          sucess: "#28b000",
+        },
+      },
+    },
   },
   plugins: [],
 }
