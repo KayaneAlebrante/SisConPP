07d865d6a724f0a2655a5188e699cf417bdce6ec
Kayane  Alebrante <alebrante.kayane22@gmail.com>
2025-01-01 23:40:19 -0300

    Fix: Rota Tela de Login
    Criação da Rota para a tela de login

--- Front-End/frontend-sisconpp/src/App.tsx
+++ Front-End/frontend-sisconpp/src/App.tsx
@@ -1,9 +1,16 @@
-import './styles/global.css'
+import { Routes, Route} from "react-router-dom";
+import { RoutesPaths } from "./models/enums/routePaths";
+import Login from "./pages/Login";
 
 function App() {
+  
   return (
-    <div></div>
-  )
+    <>
+    <Routes>
+      <Route path={RoutesPaths.Login} element={<Login />}/>
+    </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ Front-End/frontend-sisconpp/src/models/enums/routePaths.ts
@@ -0,0 +1,5 @@
+export enum RoutesPaths{
+    Login = "/login",
+}
+
+export default RoutesPaths;
