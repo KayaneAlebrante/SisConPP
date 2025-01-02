c4be808fef77f8667ef8641f4d729c8b72119b2c
Kayane  Alebrante <alebrante.kayane22@gmail.com>
2025-01-01 23:39:32 -0300

    Upatade Arquivo tailwind
    Adição de paleta de cor do sistema

--- Front-End/frontend-sisconpp/tailwind.config.js
+++ Front-End/frontend-sisconpp/tailwind.config.js
@@ -1,40 +1,41 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js, ts, jsx, tsx}"],
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Ajuste os caminhos conforme necessário
+    './public/index.html',
+  ],
   theme: {
     extend: {
-      fontFamily:{
-        sans: "Roboto, sans-serif",
-      },
-      
-      colors:{
-        green:{
+      colors: {
+        green: {
           500: "#1d3728",
           200: "#7dbc97",
         },
-
-        white:{
-          200: "#f9f3d5",
-          100: "#fcfbe5",
+        white: {
+          200: "#f9f3d5", 
+          100: "#fcfbe5", 
         },
-
-        brown:{
-          500: "#783e19",
-          400: "#c5692a",
-          300: "#b99471",
+        brown: {
+          500: "#783e19", 
+          400: "#c5692a", 
+          300: "#b99471", 
         },
-
+        purple: {
+          500: "#6d28d9", 
+          300: "#b794f4", 
+          100: "#ede9fe", 
+        },
         black: {
-          500: "#000000",
+          500: "#000000", 
         },
-
-        alertColor:{
-          erro: "#b00005",
-          sucess: "#28b000",
+        alertColor: {
+          erro: "#b00005", 
+          success: "#28b000", 
         },
       },
     },
   },
   plugins: [],
-}
+};
 
