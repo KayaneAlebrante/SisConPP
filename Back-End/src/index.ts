bb3d398116aab4eb4e04052aad93e06897cf048d
Kayane Alebrante <alebrante.kayane22@gmail.com>
2025-01-27 19:13:48 -0300

    Fix: adicionando rota para concurso

--- Back-End/src/index.ts
+++ Back-End/src/index.ts
@@ -3,6 +3,7 @@ import rtRoutes from "./routes/rt-routes";
 import ctgRoutes from "./routes/ctg-routes";
 import usuarioRoutes from "./routes/usuario-routes";
 import candidatoRoutes from "./routes/candidato-routes";
+import concursoRoutes from "./routes/concurso-routes";
 
 
 const app: Application = express(); 
@@ -14,6 +15,7 @@ app.use("/rt", rtRoutes);
 app.use("/ctg", ctgRoutes);
 app.use("/usuario", usuarioRoutes);
 app.use("/candidato", candidatoRoutes);
+app.use("/concurso", concursoRoutes);
 
 const PORT = process.env.PORT || 3002;
 
