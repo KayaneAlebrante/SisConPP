a8fc8109d04d010bbf5c7e5c4d867df62a2c0a64
Kayane Alebrante <alebrante.kayane22@gmail.com>
2025-03-01 09:51:23 -0300

    Create: ProvaService

---
+++ Back-End/src/services/prova-service.ts
@@ -0,0 +1,67 @@
+import { PrismaClient, ProvaPratica, ProvaTeorica, Recurso, TipoProva } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class ProvaService{
+    constructor(protected prisma: PrismaClient){}
+
+    async criarProva(
+        nomeProva: string,
+        tipo: TipoProva
+    ){
+        try{
+            const prova = await this.prisma.prova.create({
+                data:{
+                    nomeProva,
+                    tipo,
+                }
+            });
+
+            return prova;
+        }catch(error){
+            console.error("Erro ao criar Prova:", error);
+            throw error;
+        }
+
+    }
+
+    async atualizarProva(
+        idProva: number,
+        data: {
+            nomeProva?: string,
+            tipo?: TipoProva,
+        }
+    ){
+        try{
+            const prova = await this.prisma.prova.update({
+                where: { idProva: idProva },
+                data: data,
+            });
+            return prova;
+        }catch(error){
+            throw new Error("Erro ao atualizar Prova. Verefique os dados fornecidos.");
+        }
+    }
+
+    async buscarProvas(){
+        try{
+            const provas = await this.prisma.prova.findMany();
+            return provas;
+        }catch(error){
+            throw new Error("Erro ao buscar provas.");
+        }
+    }
+
+    async deletarProvas(idProva: number){
+        try{
+            const prova = await this.prisma.prova.delete({
+                where: { idProva: idProva },
+            });
+        }catch(error){
+            throw new Error("Erro ao deltar Prova.");
+        }
+    }
+
+}
+
+export default ProvaService;
