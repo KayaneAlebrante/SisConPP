2dee30b9f81b1e7107e1e1bc32091c215f3b70a2
Kayane Alebrante <alebrante.kayane22@gmail.com>
2025-03-09 10:56:36 -0300

    Create: SubQuesitp
    Criação do Service, Controller e Routes do SubQuesito, realizado teste de todas as rotas sem falhas.

---
+++ Back-End/src/controllers/subquesito-controller.ts
@@ -0,0 +1,79 @@
+import { Request, Response } from "express";
+import subQuesitosService from "../services/subquesito-service";
+
+
+class SubquesitoController{
+    async criarsubQuesitos( req: Request, res: Response){
+        const { nomeSubquesito, notaSubequesito, quesitoId } = req.body;
+
+        if (!nomeSubquesito || !notaSubequesito || !quesitoId){
+            return res.status(400).json({ mensagem: "Nome do Subquesito, Nota do Subquesito e ID do Quesito são obrigatórios."});
+        }
+        try{
+            const subquesito = await subQuesitosService.criarsubQuesitos(
+                nomeSubquesito,
+                notaSubequesito,
+                quesitoId
+            );
+            res.json(subquesito);
+        }catch (error) {
+            console.error("Erro ao criar Subquesito", error);
+            return res.status(400).json({ mensagem: "Erro ao criar o Subquesito. Verifique os dados fornecidos." });
+        }
+    }
+
+    async atualizarsubQuesitos( req: Request, res: Response){
+        const { id } = req.params;
+        const data = req.body;
+
+        if (!data || Object.keys(data).length === 0){
+            return res.status(400).json({ mensagem: "Dados para atualização são obrigatórios."});
+        }
+        try{
+            const subquesito = await subQuesitosService.atualizarsubQuesitos(Number(id), data);
+            return res.status(200).json(subquesito);
+        }catch (error){
+            console.error("Erro ao atualizar Subquesito", error);
+            return res.status(400).json({ mensagem: "Erro ao atualizar o Subquesito. Verifique os dados fornecidos." });
+        }
+    }
+
+    async buscarSubQuesitoPorId( req: Request, res: Response){
+        const { id } = req.params;
+
+        try{
+            const subquesito = await subQuesitosService.buscarSubQuesitoPorId(Number(id));
+            if (!subquesito){
+                return res.status(404).json({ mensagem: "Subquesito não encontrado." });
+            }
+            return res.json(subquesito);
+        }catch (error){
+            console.error("Erro ao buscar Subquesito", error);
+            return res.status(400).json({ mensagem: "Erro ao buscar o Subquesito. Verifique os dados fornecidos." });
+        }
+    }
+
+    async buscarSubQuesitos( req: Request, res: Response){
+        try{
+            const subquesitos = await subQuesitosService.buscarSubQuesitos();
+            return res.json(subquesitos);
+        }catch (error){
+            console.error("Erro ao buscar Subquesitos", error);
+            return res.status(400).json({ mensagem: "Erro ao buscar os Subquesitos. Verifique os dados fornecidos." });
+        }
+    }
+
+    async deletarSubQuesito( req: Request, res: Response){
+        const { id } = req.params;
+
+        try{
+            await subQuesitosService.deletarSubQuesito(Number(id));
+            return res.json({ mensagem: "Subquesito deletado com sucesso." });
+        }catch (error){
+            console.error("Erro ao deletar Subquesito", error);
+            return res.status(400).json({ mensagem: "Erro ao deletar o Subquesito. Verifique os dados fornecidos." });
+        }
+    }
+}
+
+export default new SubquesitoController();
--- Back-End/src/services/subquesito-service.ts
+++ Back-End/src/services/subquesito-service.ts
@@ -28,6 +28,46 @@ class SubQuesitosService{
 
     }
 
+    async atualizarsubQuesitos(idSubequestios: number, data: any){
+        try{
+            const subquesito = await this.prisma.subQuesitos.update({
+                where: { idSubequestios },
+                data,
+            });
+
+            console.log("Subquesito atualizado com sucesso.");
+            return subquesito;
+        }catch (error){
+            console.error("Erro detalhado:", error);
+            throw new Error("Erro ao atualizar Subquesito. Verifique os dados fornecidos.");
+        }
+    }
+
+    async buscarSubQuesitoPorId(idSubequestios: number){
+        const subquesito = await this.prisma.subQuesitos.findUnique({
+            where: { idSubequestios },
+        });
+
+        return subquesito;
+    }
+
+    async buscarSubQuesitos(){
+        const subquesitos = await this.prisma.subQuesitos.findMany();
+        return subquesitos;
+    }
+
+    async deletarSubQuesito(idSubequestios: number){
+        try{
+            await this.prisma.subQuesitos.delete({
+                where: { idSubequestios },
+            });
+
+            console.log("Subquesito deletado com sucesso.");
+        }catch (error){
+            console.error("Erro detalhado:", error);
+            throw new Error("Erro ao deletar Subquesito.");
+        }
+    }
 }
 
 const subQuesitosService = new SubQuesitosService(prisma);
